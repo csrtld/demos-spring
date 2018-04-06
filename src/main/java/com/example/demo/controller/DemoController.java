07cde9c1c6dbd58c09624fc2af873ff908ec8087
Cesar Toledo <ctoledo@localhost.localdomain>
2018-04-06 00:07:26 -0300

    custom source vault

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,11 @@ public class DemoController {
     @Autowired
     Environment env;
 
+    @Value("${nombre}")
+    String quien;
+
     @GetMapping(value = "/saludo")
     public String methodVault() {
-        String quien = env.getProperty("nombre");
         return "salida: " + env.getProperty("saludo") + " " + quien;
     }
 
